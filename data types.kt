a68e1833eac5c532f6f9612f6ac7beeda7d826a0
Sarakhairy <116834131+Sarakhairy@users.noreply.github.com>
2025-11-09 19:18:42 +0200

    Second exercise

---
+++ data types.kt
@@ -0,0 +1,9 @@
+fun main(){
+  var age = 22
+  var city  = "Luxor"
+  var likeKotlin  = false 
+  println("My age is $age")
+  println("My city is $city")
+  println("do i like kotlin ? $likeKotlin")
+  
+}
